034902e87ec536618b0f33aeb663ec9fbda9d07b
AdrienneD <adriennedao@gmail.com>
2017-08-27 13:17:20 -0400

    made changes

--- script.js
+++ script.js
@@ -7,20 +7,18 @@ var instruction = prompt("You may begin by clicking on the card next, click Ok!"
 var userResponse = "click ok";
 
 function myFunction() {
-<<<<<<< HEAD
     document.getElementById("quiz").innerHTML = "Le Mont-Saint-Michel is an island commune in Normandy, France. Note: Flip the card";
 }
 
 //method returns the element that has the ID attribute with the specified value.
 var card = document.getElementById('card');
 
-=======
     document.getElementById("quiz").innerHTML = "July 14, 1789, the storming of Bastille Day";
 }
 //method returns the element that has the ID attribute with the specified value.
 var card = document.getElementById('card');
 
->>>>>>> 045e8e07f65df12d63a785e8cf860afc98fec1b8
+
 //function for the card
 card.addEventListener('click', function() {
     if (!this.classList.contains('on')) {
